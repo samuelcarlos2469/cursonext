4a106e02b86faf7231e6cff58cb0678ed96f2d03
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2024-08-21 21:29:01 -0400

    excluindo oq faltava kkkk

--- public/components/header.tsx
+++ public/components/header.tsx
@@ -7,14 +7,3 @@ import header from "../style/header.module.css";
 import logo from "../img/logo.png";
 
 //componentes começam em Maiuscula para diferenciação dos elementos nativos do HTML
-export default function Header() {
-  //pode ser importado sem chaves em outros arquivos
-  return (
-    //aplica a classe CSS header definida no arquivo header.module.css.
-    //O header.header refere-se ao estilo, o primeiro header é o nome do CSS
-    //e o segundo header é a classe dentro desse arquivo.
-    <header className={header.header}>
-      <Image src={logo} height={100} width={300} alt="Logo da API utilizada" />
-    </header>
-  );
-}
